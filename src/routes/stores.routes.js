7c62f314ba32aaac1692ffaa38535c87efac2832
I7RANK <franfgh2000@hotmail.com>
2025-08-22 16:24:54 -0500

    feat: add route for update current code

--- src/routes/stores.routes.js
+++ src/routes/stores.routes.js
@@ -7,6 +7,9 @@ router.post('/', async (req, res) => {
   console.log('/stores', req.body);
   try {
     const newStore = new Store(req.body);
+    if (!newStore.currentCode?.createdAt && newStore.currentCode?.code) {
+      newStore.currentCode.createdAt = new Date();
+    }
     const savedStore = await newStore.save();
     res.status(201).json(savedStore);
   } catch (error) {
@@ -15,6 +18,41 @@ router.post('/', async (req, res) => {
     }
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.patch('/:id/code', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { code } = req.body;
+
+    if (!code) {
+      return res.status(400).json({ message: "The 'code' field is required" });
+    }
+
+    const store = await Store.findById(id);
+    if (!store) {
+      return res.status(404).json({ message: 'Store not found' });
+    }
+
+    if (store.currentCode?.code) {
+      store.codesHistory.push({
+        code: store.currentCode.code,
+        createdAt: store.currentCode?.createdAt,
+      });
+    }
+
+    store.currentCode = { code, createdAt: new Date() };
+
+    await store.save();
+
+    res.json({
+      message: 'Code updated successfully!!',
+      store,
+    });
+  } catch (error) {
+    console.error('Error updating the code:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 });
 
